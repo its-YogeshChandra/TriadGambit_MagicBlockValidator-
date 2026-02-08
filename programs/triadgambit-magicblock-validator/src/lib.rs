7eadc1b2be024b6eb86b38a7dba724b3129775f5
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-09 03:27:59 +0530

    added init and play game struct

--- programs/triadgambit-magicblock-validator/src/lib.rs
+++ programs/triadgambit-magicblock-validator/src/lib.rs
@@ -10,7 +10,42 @@ pub mod triadgambit_magicblock_validator {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn play_game(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+#[account]
+#[derive(InitSpace)]
+pub struct GameAccountShape {
+    #[max_len(50)]
+    user_selected_element: String,
+
+    #[max_len(50)]
+    user_received_element: String,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    //the program to fix the thing
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(init, payer = signer, space = 8 + GameAccountShape::INIT_SPACE , seeds = [b"user_game_account", signer.key().as_ref()], bump)]
+    pub user_game_account: Account<'info, GameAccountShape>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct PlayGame<'info> {
+    //the program to fix the thing
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(mut)]
+    pub user_game_account: Account<'info, GameAccountShape>,
+
+    pub system_program: Program<'info, System>,
+}
